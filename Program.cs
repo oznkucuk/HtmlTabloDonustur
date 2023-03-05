ce8190578de36c7d9761073f969f8d4ae7abd8de
ozan <oznkucuk@gmail.com>
2023-03-05 13:12:51 +0300

    json dosyası okunup test edildi

--- Program.cs
+++ Program.cs
@@ -4,4 +4,11 @@ var jsoncevir = File.OpenText("movies.json");
 var movies = JsonSerializer.Deserialize<List<Movie>>(jsoncevir.ReadToEnd(), new JsonSerializerOptions
 { PropertyNameCaseInsensitive = true });
 
-
+foreach (var item in movies)
+{
+    Console.WriteLine(item.Id);
+    Console.WriteLine(item.Title);
+    Console.WriteLine(item.Rating);
+    Console.WriteLine(item.Genre);
+    Console.WriteLine(item.Duration);
+}

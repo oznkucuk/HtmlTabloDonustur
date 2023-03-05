363e5c4fa0c5163ea77ff74841db383be6249d2e
ozan <oznkucuk@gmail.com>
2023-03-05 13:12:18 +0300

    de-serialize yapıldı

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var jsoncevir = File.OpenText("movies.json");
+var movies = JsonSerializer.Deserialize<List<Movie>>(jsoncevir.ReadToEnd(), new JsonSerializerOptions
+{ PropertyNameCaseInsensitive = true });
+
+

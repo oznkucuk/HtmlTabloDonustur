59f5a74f9949d6da4b5885b94c442e381ebf049e
ozan <oznkucuk@gmail.com>
2023-03-05 13:09:48 +0300

    proplar tanımlandı

--- Movie.cs
+++ Movie.cs
@@ -1,5 +1,9 @@
 // See https://aka.ms/new-console-template for more information
 class Movie
 {
-
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Rating { get; set; }
+    public string Genre { get; set; }
+    public int Duration { get; set; }
 }
